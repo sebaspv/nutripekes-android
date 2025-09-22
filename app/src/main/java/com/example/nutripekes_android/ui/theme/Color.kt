36b651ac1198b47a2ef9b35ae338f707a24be8cc
sebaspv <poncegramming@gmail.com>
2025-09-21 18:19:04 -0600

    add font and custom colours

--- app/src/main/java/com/example/nutripekes_android/ui/theme/Color.kt
+++ app/src/main/java/com/example/nutripekes_android/ui/theme/Color.kt
@@ -9,3 +9,17 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val GreyPeke = Color(0xFF5D5C60)
+
+val YellowPeke = Color(0xFFF1981F)
+
+val OrangePeke = Color(0xFFE87523)
+
+val LightGreenPeke = Color(0xFF55AA2A)
+
+val GreenPeke = Color(0xFF255A2E)
+
+val RedPeke = Color(0xFFDF1E34)
+
+val PinkPeke = Color(0xFFE55B57)
